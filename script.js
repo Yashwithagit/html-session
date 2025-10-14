32b1b5eee46ee9cbd4304eb9289eb19b260f3c1c
intra9ine <yashwitha@intranine.com>
2025-10-14 17:18:18 +0400

    FEAT:js variable

---
+++ script.js
@@ -0,0 +1,18 @@
+// let
+let a=10;
+console.log('constant value a is ',a);
+a=20
+console.log('constant value a is ',a);
+
+// const
+// const a=10;
+// console.log('constant value a is ',a) 
+// a=20
+// console.log('constant value a is ',a)
+
+// var
+// var myVar=10;
+// console.log(myVar)
+// myVar=30
+// console.log(myVar)
+
