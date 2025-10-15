772c1527cc5619e9799fe7d5bdf628ad454f2b09
intra9ine <yashwitha@intranine.com>
2025-10-15 16:54:20 +0400

    FEAT:DATA TYPES

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // let
-let a=10;
-console.log('constant value a is ',a);
-a=20
-console.log('constant value a is ',a);
+// let a=10;
+// console.log('constant value a is ',a);
+// a=20
+// console.log('constant value a is ',a);
 
 // const
 // const a=10;
@@ -15,4 +15,64 @@ console.log('constant value a is ',a);
 // console.log(myVar)
 // myVar=30
 // console.log(myVar)
+
+
+// Data types
+// 1.Primitive data types
+// Number()
+10,10.2
+// console.log(parseInt("10"))
+
+// String
+// const myName="  Cat xyz "
+// console.log(`My name is ${myName} _____`)
+// const newName=myName.trimStart()
+// console.log(newName.length)
+// console.log(myName.length)
+
+// concatenations
+// const lastName="My Name is xyz"
+// console.log(lastName.toUpperCase())
+// console.log(myName.concat("jsdjsd",lastName))
+
+// Boolean
+// const isApproved=true
+// console.log(typeof(isApproved))
+
+// undefined
+// let newVar=undefined;
+// console.log(newVar)
+
+// null
+// let newVar="null";
+// newVar=null
+// console.log(typeof newVar)
+
+// Symbol
+// const mySymbol=Symbol('id')
+// console.log(mySymbol)
+
+// 2.Reference data types Or Non Primitive data types
+// Array
+const array=[1,3,4,5,6,6,3]
+// Object
+const myObj={x:10,y:30}
+// Function
+// arrow function
+const square=(number,value=10)=>{
+console.log(number*2,value)
+}
+square(20)
+
+// anonymous function
+// const newfunc=(function (age){
+//    return age>18
+// })
+// console.log(newfunc(15))
+
+
+
+
+// Sets
+// console.log([...new Set(array)])
 
